de3fc163003642e7e759af3930013552f52dbe8f
Maksym Diachenko <max97292@gmail.com>
2021-12-01 16:14:13 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// image for the pokemon
+// https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png
 
-export default App;
+const pokemon = [
+  { id: 4, name: 'charizard' },
+  { id: 10, name: 'caterpie' },
+  { id: 77, name: 'ponyta' },
+  { id: 108, name: 'lickitung' },
+  { id: 132, name: 'ditto' },
+  { id: 133, name: 'eevee' },
+];
+
+export default function App() {
+  return <div className="app"></div>;
+}
